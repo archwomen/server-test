c9c26a13f7bd211fbf91c6e284139061ef132f77
root <root@localhost.localdomain>
2012-12-18 00:32:05 -0500

    first commit

---
+++ perl/index.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+print "Content-Type: text/html\n\n";
+print "<HTML><head><title>Test Page</title></head>\n";
+print ("<body><h1>Hello World</h1>\n");
+print "<p>Setup is Successful.</p></body></html>";
+exit (1);
